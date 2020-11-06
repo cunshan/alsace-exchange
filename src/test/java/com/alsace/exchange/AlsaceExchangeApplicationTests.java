feb40609340d3151d3d8afdfa0183b44f0a30936
wangcunshan <wangcunshan62@camelotchina.com>
2020-11-06 14:41:51 +0800

    初始化

---
+++ src/test/java/com/alsace/exchange/AlsaceExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alsace.exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlsaceExchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
