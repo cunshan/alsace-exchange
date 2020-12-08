4cf4b0f2c95a656b40651531e37d25e1080e4202
wangcunshan <wangcunshan62@camelotchina.com>
2020-12-08 14:49:46 +0800

    初始化 user查询优化

--- alsace-exchange-service/src/main/java/com/alsace/exchange/service/user/service/impl/UserServiceImpl.java
+++ alsace-exchange-service/src/main/java/com/alsace/exchange/service/user/service/impl/UserServiceImpl.java
@@ -1,20 +1,30 @@
 package com.alsace.exchange.service.user.service.impl;
 
 import com.alsace.exchange.common.annontation.AutoFill;
+import com.alsace.exchange.common.base.Constants;
 import com.alsace.exchange.common.base.PageHelper;
 import com.alsace.exchange.common.base.PageParam;
 import com.alsace.exchange.common.base.PageResult;
 import com.alsace.exchange.common.enums.AutoFillType;
+import com.alsace.exchange.common.exception.AlsaceException;
 import com.alsace.exchange.service.user.domain.User;
 import com.alsace.exchange.service.user.repositories.UserRepository;
+import com.alsace.exchange.service.user.repositories.UserSpecs;
 import com.alsace.exchange.service.user.service.UserService;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -38,16 +48,19 @@ public class UserServiceImpl implements UserService {
   @AutoFill(AutoFillType.CREATE)
   @Transactional(rollbackFor = Exception.class)
   public User save(User param) {
+    User userParam = new User();
+    userParam.setLoginAccount(param.getLoginAccount()).setDeleted(false);
+    long userCount = userRepository.count(Example.of(userParam));
+    if (userCount > 0) {
+      throw new AlsaceException(String.format("用户名%s已经存在！", param.getLoginAccount()));
+    }
     param.setLocked(false);
     return userRepository.saveAndFlush(param);
   }
 
   @Override
-  @AutoFill(AutoFillType.CREATE)
-  @Transactional(rollbackFor = Exception.class)
   public List<User> save(List<User> param) {
-    param.forEach(user -> user.setLocked(false));
-    return userRepository.saveAll(param);
+    throw new UnsupportedOperationException(Constants.UNSUPPORTED_OPERATION);
   }
 
   @Override
