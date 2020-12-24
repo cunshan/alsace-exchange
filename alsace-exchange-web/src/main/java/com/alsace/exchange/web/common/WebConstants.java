c53f380b536873928b391b873738cc5352c2f132
wangcunshan <wangcunshan62@camelotchina.com>
2020-12-24 20:57:03 +0800

    调整token超时时间为24小时

--- alsace-exchange-web/src/main/java/com/alsace/exchange/web/common/WebConstants.java
+++ alsace-exchange-web/src/main/java/com/alsace/exchange/web/common/WebConstants.java
@@ -5,9 +5,9 @@ public class WebConstants {
   public static final String JWT_ERROR_MSG = "登录信息超时，请重新登录！";
 
   /**
-   * token超时时间
+   * token超时时间 分钟
    */
-  public static final long JWT_TOKEN_TIMEOUT = 60L;
+  public static final long JWT_TOKEN_TIMEOUT = 1440L;
 
   public static final String SHIRO_CACHE_PREFIX = "alsace:shiro:cache:";
 
