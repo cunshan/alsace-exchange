34f3ca57fc54b4fc18dedbde753eed4733f70f43
wangcunshan <wangcunshan62@camelotchina.com>
2020-12-29 13:59:51 +0800

    调整repository注入方式

--- alsace-exchange-common/src/main/java/com/alsace/exchange/common/base/AbstractBaseServiceImpl.java
+++ alsace-exchange-common/src/main/java/com/alsace/exchange/common/base/AbstractBaseServiceImpl.java
@@ -11,24 +11,13 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.util.List;
 
 public abstract class AbstractBaseServiceImpl<T extends BaseEntity> implements BaseService<T, Long> {
 
-  protected JpaRepository<T, Long> jpaRepository;
-
-  protected JpaSpecificationExecutor<T> specificationExecutor;
   @Resource
   protected LoginInfoProvider loginInfoProvider;
-
-
-  @PostConstruct
-  public void init(){
-    this.jpaRepository = getJpaRepository();
-    this.specificationExecutor = getJpaSpecificationExecutor();
-  }
 
   protected abstract JpaRepository<T, Long> getJpaRepository();
 
@@ -51,25 +40,25 @@ public abstract class AbstractBaseServiceImpl<T extends BaseEntity> implements B
   @Override
   public T findOne(T t) {
     Example<T> example = Example.of(t);
-    return jpaRepository.findOne(example).orElse(null);
+    return getJpaRepository().findOne(example).orElse(null);
   }
 
   @Override
   public T getOneById(Long id) {
     Assert.notNull(id, Constants.ID_NOT_NULL_ERROR);
-    return jpaRepository.findById(id).orElse(null);
+    return getJpaRepository().findById(id).orElse(null);
   }
 
   @Override
   @AutoFill(AutoFillType.CREATE)
   @Transactional(rollbackFor = Exception.class)
   public T save(T param) {
-    return jpaRepository.saveAndFlush(param);
+    return getJpaRepository().saveAndFlush(param);
   }
 
   @Override
   public List<T> saveBatch(List<T> param) {
-    return jpaRepository.saveAll(param);
+    return getJpaRepository().saveAll(param);
   }
 
   @Override
@@ -80,16 +69,16 @@ public abstract class AbstractBaseServiceImpl<T extends BaseEntity> implements B
     T dbUser = this.getOneById(param.getId());
     Assert.state(dbUser != null, Constants.UPDATE_NOT_EXISTS_ERROR);
     AlsaceBeanUtils.copyNotNullProperties(param, dbUser);
-    return jpaRepository.saveAndFlush(dbUser);
+    return getJpaRepository().saveAndFlush(dbUser);
   }
 
   @Override
   public PageResult<T> findPage(PageParam<T> param) {
     Page<T> userPage;
     if (param.getParam() == null) {
-      userPage = jpaRepository.findAll(PageHelper.page(param));
+      userPage = getJpaRepository().findAll(PageHelper.page(param));
     } else {
-      userPage = jpaRepository.findAll(Example.of(param.getParam()), PageHelper.page(param));
+      userPage = getJpaRepository().findAll(Example.of(param.getParam()), PageHelper.page(param));
     }
     return new PageResult<>(userPage);
   }
@@ -100,15 +89,15 @@ public abstract class AbstractBaseServiceImpl<T extends BaseEntity> implements B
     T dbUser = this.getOneById(id);
     Assert.state(dbUser != null, Constants.DELETE_NOT_EXISTS_ERROR);
     dbUser.setDeleted(true);
-    jpaRepository.saveAndFlush(dbUser);
+    getJpaRepository().saveAndFlush(dbUser);
     return true;
   }
 
   @Override
   public List<T> findAll(T param) {
     if (param == null) {
-      return jpaRepository.findAll();
+      return getJpaRepository().findAll();
     }
-    return jpaRepository.findAll(Example.of(param));
+    return getJpaRepository().findAll(Example.of(param));
   }
 }
