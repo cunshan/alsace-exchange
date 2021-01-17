0131fe9d491b2cc90dfef0220aff14d5638982b2
wangcunshan <wangcunshan62@camelotchina.com>
2021-01-17 22:14:59 +0800

    人员检测任务结果录入

--- alsace-exchange-service/src/main/java/com/alsace/exchange/service/detection/service/PersonTaskDetailService.java
+++ alsace-exchange-service/src/main/java/com/alsace/exchange/service/detection/service/PersonTaskDetailService.java
@@ -4,6 +4,7 @@ import com.alsace.exchange.common.base.BaseService;
 import com.alsace.exchange.service.detection.domain.PersonTaskDetail;
 import com.alsace.exchange.service.sys.domain.User;
 
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,5 +22,14 @@ public interface PersonTaskDetailService extends BaseService<PersonTaskDetail,Lo
      * @param taskCodeList 任务编码列表
      * @param positive 是否阳性
      */
-    void updateResult(List<String> taskCodeList, Boolean positive);
+    void updateResultBatch(List<String> taskCodeList, Boolean positive);
+
+    /**
+     * 根据试管编码更新
+     * @param taskCode 任务编码
+     * @param testTubeNo 试管编码
+     * @param detectionMethod 检测项目  1：核酸检测 2：抗体检测
+     * @param positive 是否阳性
+     */
+    void updateResultSingle(String taskCode, String testTubeNo, Integer detectionMethod, Boolean positive);
 }
