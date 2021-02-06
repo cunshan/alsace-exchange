d35499c68e20aa40971eb43bbd98fc211e806214
wangcunshan <wangcunshan62@camelotchina.com>
2021-02-06 16:53:57 +0800

    APP接口  环境检测明细提交

--- alsace-exchange-service/src/main/java/com/alsace/exchange/service/detection/service/impl/EnvironmentTaskServiceImpl.java
+++ alsace-exchange-service/src/main/java/com/alsace/exchange/service/detection/service/impl/EnvironmentTaskServiceImpl.java
@@ -240,13 +240,16 @@ public class EnvironmentTaskServiceImpl extends AbstractBaseServiceImpl<Environm
     Assert.state(dbDetail != null, String.format("任务明细不存在！[%s]", param.getDetailCode()));
     Assert.state(TaskDetailStatus.INIT.status().equals(dbDetail.getDetailStatus()), String.format("明细已经提交！[%s]", dbDetail.getDetailCode()));
     //任务明细改成已提交
-    dbDetail.setDetailStatus(TaskDetailStatus.SUBMITTED.status());
-    dbDetail.setDetectionDate(new Date());
-    this.environmentTaskDetailService.save(dbDetail);
+    EnvironmentTaskDetail updateDetail = new EnvironmentTaskDetail();
+    updateDetail.setId(dbDetail.getId());
+    updateDetail.setDetailStatus(TaskDetailStatus.SUBMITTED.status());
+    updateDetail.setDetectionDate(new Date());
+    this.environmentTaskDetailService.update(dbDetail);
     List<EnvironmentTaskDetailResult> resultList = param.getDetailResultList();
     // 提交检测明细样本要在3条之内
-    Assert.state(resultList.size() > 3, "检测样本数量超过3个！");
+    Assert.state(resultList.size() <= 3, "检测样本数量超过3个！");
     resultList.forEach(result -> {
+      Assert.hasLength(result.getDetailType(),"样本类型为空！");
       if (EnvironmentTaskDetailResultType.FOOD.status().equals(result.getDetailType())) {
         //食物的要求3个字段必填
         Assert.hasLength(result.getManufacturePlace(), "产地为空！");
